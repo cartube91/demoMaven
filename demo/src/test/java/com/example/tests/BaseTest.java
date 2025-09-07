66359c3ac70663e52643f79fb1cf657b69696edf
Nad <ggg@huawei_gr3_2017-bee0c5de7.home>
2025-09-07 15:33:38 +0300

    msg

--- demo/src/test/java/com/example/tests/BaseTest.java
+++ demo/src/test/java/com/example/tests/BaseTest.java
@@ -1,32 +1,42 @@
-package com.example.tests;
+    package com.example.tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+    import org.testng.annotations.AfterMethod;
+    import org.testng.annotations.BeforeMethod;
 
-public class BaseTest {
-
-    
-    
-
-    protected WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if(driver != null) {
-            driver.quit();
-        }
+    public class BaseTest {
 
         
-    }
+        
+
+        protected WebDriver driver;
+
+        @BeforeMethod
+        public void setUp() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");        // run headless
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");          // required for Linux CI
+    options.addArguments("--disable-dev-shm-usage"); // avoid /dev/shm issues
+    options.addArguments("--window-size=1920,1080");
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--user-data-dir=/tmp/chrome-" + java.util.UUID.randomUUID()); // unique profile
+
+    driver = new ChromeDriver(options);
+    driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
 }
+
+        @AfterMethod
+        public void tearDown() {
+            if(driver != null) {
+                driver.quit();
+            }
+
+            
+        }
+    }
